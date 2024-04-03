0e4de85c96af3eb969cb742a066f8560d37ed44c
ftelias <ftenorioelias@gmail.com>
2024-04-03 16:21:26 +1000

    Update HUM.py

--- HUM.py
+++ HUM.py
@@ -295,16 +295,16 @@ def calculate_bulk_prices():
 
         # Calculate values for energy_rates DataFrame based on user-selected state
     if not st.session_state['updated_df'].empty:
-        available_years = st.session_state['updated_df'].index.tolist()
+        available_years = [str(year) for year in st.session_state['updated_df'].index.tolist()]
 
         for year in available_years:
 
             # Summary of Rates
 
             # Fetch values from the updated_df DataFrame for the selected state
-            peak_rate = st.session_state['updated_df'][selected_state].iloc[year - 1]
-            shoulder_rate = st.session_state['updated_df'][selected_state].iloc[year - 1]
-            off_peak_rate = st.session_state['fetched_data'][selected_state].iloc[year - 1]/10
+            peak_rate = st.session_state['updated_df'].loc[year, selected_state]
+            shoulder_rate = st.session_state['updated_df'].loc[year, selected_state]
+            off_peak_rate = st.session_state['fetched_data'].loc[year, selected_state] / 10
 
             # Format the values as floats with three decimals
             #peak_rate = f"{peak_rate:.3f}"
@@ -418,7 +418,7 @@ def calculate_bulk_prices():
     # Function to calculate the average for last column from Years 1 through 4
     def calculate_year_5_average(df):
         for factor in range(len(df)):
-            year_values = [df.at[factor, f'Year {year}'] for year in range(1, 5) if f'Year {year}' in df.columns]
+            year_values = [df.at[factor, f'Year {year}'] for year in available_years if f'Year {year}' in df.columns]
             if year_values:
                 average_value = sum(year_values) / len(year_values)
                 df.at[factor, 'Average'] = average_value
@@ -434,7 +434,6 @@ def calculate_bulk_prices():
 
     return energy_rates, summary_of_consumption, summary_of_charges, summary_of_costs, summary_of_rates, selected_state, bulk_price
 
-    
 #########################################################################################################
 #########################################################################################################
 # DISPLAY SUMMARY TABLES
